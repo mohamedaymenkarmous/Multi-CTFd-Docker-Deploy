adef0d5936be7955002f8521d6714fa1b375c84a
mohamedaymenkarmous <mohamed.aymen.karmous@gmail.com>
2020-03-25 19:52:53 +0100

    updating the script to python3

--- 2-create-new-projects-docker.sh
+++ 2-create-new-projects-docker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-n=$(cat config.json |python -c "import json,sys;print len(json.load(sys.stdin)['projects'])")
+n=$(cat config.json |python3 -c "import json,sys;print(len(json.load(sys.stdin)['projects']))")
 
 if [ "$n" == "0" ]; then
   echo "Skipped"
 fi
 
 for i in $(seq 1 $n);do
-  name=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['name']")
-  hostname=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['hostname']")
-  generic_hostname=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['generic-hostname']")
-  tls_enabled=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['tls-enabled']")
+  name=$(cat config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['name'])")
+  hostname=$(cat config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['hostname'])")
+  generic_hostname=$(cat config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['generic-hostname'])")
+  tls_enabled=$(cat config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['tls-enabled'])")
   #ls docker-compose-files/$name.yml &>/dev/null || (
     sed "s/REPLACED_NAME/${name}/g" <docker-compose-merged-template.yml > docker-compose-files/dcf-${name}.yml;
     ln -sf ${PWD}/docker-compose-files/dcf-${name}.yml ${PWD}/CTFd/dcf-${name}.yml
