303b913f8da11670ae36b7dd714fe22f0d8675e4
mohamedaymenkarmous <mohamed.aymen.karmous@gmail.com>
2020-03-25 19:55:11 +0100

    updating the script to python3

--- 4-create-new-projects-nginx.sh
+++ 4-create-new-projects-nginx.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-n=$(cat config.json |python -c "import json,sys;print len(json.load(sys.stdin)['projects'])")
+n=$(cat config.json |python3 -c "import json,sys;print len(json.load(sys.stdin)['projects']))")
 
 if [ "$n" == "0" ]; then
   echo "Skipped"
 fi
 
 for i in $(seq 1 $n);do
-  name=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['name']")
-  hostname=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['hostname']")
-  generic_hostname=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['generic-hostname']")
-  tls_enabled=$(cat config.json |python -c "import json,sys;print json.load(sys.stdin)['projects'][${i}-1]['tls-enabled']")
+  name=$(cat config.json |pytho3n -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['name'])")
+  hostname=$(cat config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['hostname'])")
+  generic_hostname=$(cat config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['generic-hostname'])")
+  tls_enabled=$(cat config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['projects'][${i}-1]['tls-enabled'])")
   if [ "$tls_enabled" == "1" ]; then
     #ls nginx/sites-enabled/$name.conf &>/dev/null || (
       sed "s/REPLACED_NAME/${name}/g" <templates/nginx-vhost-template-https.conf > nginx/sites-enabled/${name}.conf.tmp;
