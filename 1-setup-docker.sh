bdcc90205d657d3a7ace7f100829fa6e45099c03
mohamedaymenkarmous <mohamed.aymen.karmous@gmail.com>
2021-04-09 20:00:29 +0100

    Make the docker compose version always the latest

--- 1-setup-docker.sh
+++ 1-setup-docker.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
-echo ""
-read -p "Before you run, check the latest compose version at: https://docs.docker.com/compose/install/"
 
+cd
+sudo apt-get update
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update -y
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-echo ""
-echo "The fingerprint above should match:"
-echo "pub   rsa4096 2017-02-22 [SCEA]"
-echo "      9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
-echo "uid           [ unknown] Docker Release (CE deb) <docker@docker.com>"
-echo "sub   rsa4096 2017-02-22 [S]"
-echo ""
-read -p ""
-
-echo ""
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+    gnupg \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker ubuntu
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# get latest docker compose released tag
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+sudo systemctl status docker
