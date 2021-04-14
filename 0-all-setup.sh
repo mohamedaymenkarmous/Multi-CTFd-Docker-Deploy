7b212a00ad478d455240de655ca041983c726ff4
mohamedaymenkarmous <mohamed.aymen.karmous@gmail.com>
2021-04-14 23:26:58 +0000

    fixing a but in the main script

--- 0-all-setup.sh
+++ 0-all-setup.sh
@@ -39,33 +39,6 @@ ls ${PWD}/CTFd/themes/core/static/img/logo.png.bak &>/dev/null || mv ${PWD}/CTFd
 ls ${PWD}/CTFd/themes/core/static/img/favicon.ico.bak &>/dev/null || mv ${PWD}/CTFd/themes/core/static/img/favicon.ico ${PWD}/CTFd/themes/core/static/img/favicon.ico.bak
 ln -sf ${PWD}/../logo.png ${PWD}/CTFd/themes/core/static/img/logo.png
 ln -sf ${PWD}/../favicon.ico ${PWD}/CTFd/themes/core/static/img/favicon.ico
-
-common_proxy_conf_path=$(cat ../config.json |python3 -c "import json,sys;print(json.load(sys.stdin)['common']['proxy_conf_path'])"  2>/dev/null)
-if [ ! -z "$common_proxy_conf_path" ]; then
-  cd ..
-  if [ -d "$common_proxy_conf_path" ]; then
-    real_path=$(readlink -f $common_proxy_conf_path)
-  else
-    echo "Error: File path specific in config.json:common.proxy_conf_path is not a directory";exit
-  fi
-  cd - > /dev/null
-fi
-
-sum=""
-for x in $(ls -1 dcf-*.yml | grep -v dcf-docker-compose-common.yml);do
-  sum="${sum} -f ${x}"
-done
-if [ ! -z "$real_path" ]; then
-  dcf_common="-f $real_path/docker-compose-common.yml"
-  if [ ! -f "$real_path/docker-compose-common.yml" ]; then
-    $real_path/2-create-new-projects-docker.sh
-  fi
-else
-  dcf_common="-f dcf-docker-compose-common.yml"
-fi
-
+cd - > /dev/null
 echo "### Building the containers"
-sudo docker-compose ${sum} --compatibility up -d --force-recreate #--build
-# It's possible to use the depends_on parameter in docker-compose file in the proxy service to make it wait for the
-#   creation of the other containers but it's hard to manage when it comes to adding and removing the container names from that parameter
-sudo docker-compose ${dcf_common} --compatibility up -d --force-recreate
+./6-rebuild-containers.sh
